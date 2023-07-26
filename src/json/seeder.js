075d6f0d9f4781b8e583322d81a8c6fa61f6bc61
Tran Quoc Tuan <tuantran1295@MacBook-Air-cua-Tran.local>
2023-07-26 18:31:59 +0700

    add mock data

--- src/json/seeder.js
+++ src/json/seeder.js
@@ -206,5 +206,265 @@
                 image: 'https://picsum.photos/182/264'
             }
         ]
+    },
+    {
+        id: '{{objectId()}}',
+        keyword: 'top ',
+        suggestionTerms: [
+            {
+                term: 'cotton top',
+                url: '#'
+            },
+            {
+                term: 'top sportwear',
+                url: '#'
+            },
+            {
+                term: 'red top',
+                url: '#'
+            },
+            {
+                term: 'green top',
+                url: '#'
+            }
+        ],
+        collections: [
+            {
+                id: '{{objectId()}}',
+                title: 'Tops',
+                url: '#'
+            }
+        ],
+        products: [
+            {
+                id: '{{objectId()}}',
+                title: 'Satin Top',
+                brand: 'Forever 21',
+                price: '$15.50',
+                image: 'https://picsum.photos/182/264'
+            },
+            {
+                id: '{{objectId()}}',
+                title: 'Black Cotton Top',
+                brand: 'Gucci',
+                price: '$64.68',
+                image: 'https://picsum.photos/182/264'
+            },
+            {
+                id: '{{objectId()}}',
+                title: 'Trumpet Sleeve Top',
+                brand: 'Gucci',
+                price: '$41.54',
+                image: 'https://picsum.photos/182/264'
+            }
+        ]
+    },
+    {
+        id: '{{objectId()}}',
+        keyword: 'top t',
+        suggestionTerms: [
+            {
+                term: 'top tank',
+                url: '#'
+            },
+            {
+                term: 'top tank for woman',
+                url: '#'
+            },
+            {
+                term: 'top tank for men',
+                url: '#'
+            },
+            {
+                term: 'top teen',
+                url: '#'
+            }
+        ],
+        collections: [
+            {
+                id: '{{objectId()}}',
+                title: 'Tops',
+                url: '#'
+            }
+        ],
+        products: [
+            {
+                id: '{{objectId()}}',
+                title: 'Essentials Women Slim-Fit Tank',
+                brand: 'GAP',
+                price: '$5.50',
+                image: 'https://picsum.photos/182/264'
+            },
+            {
+                id: '{{objectId()}}',
+                title: 'Top Men Slim-Fit',
+                brand: 'Gucci',
+                price: '$6.68',
+                image: 'https://picsum.photos/182/264'
+            },
+            {
+                id: '{{objectId()}}',
+                title: 'Fruit of the Loom Girls Top',
+                brand: 'Gucci',
+                price: '$25.54',
+                image: 'https://picsum.photos/182/264'
+            }
+        ]
+    },
+    {
+        id: '{{objectId()}}',
+        keyword: 'top ta',
+        suggestionTerms: [
+            {
+                term: 'top tank with bra',
+                url: '#'
+            },
+            {
+                term: 'top tank for woman',
+                url: '#'
+            },
+            {
+                term: 'top tank for men',
+                url: '#'
+            },
+            {
+                term: 'top tablets',
+                url: '#'
+            }
+        ],
+        collections: [
+            {
+                id: '{{objectId()}}',
+                title: 'Tops',
+                url: '#'
+            }
+        ],
+        products: [
+            {
+                id: '{{objectId()}}',
+                title: 'Essentials Women Slim-Fit Tank',
+                brand: 'GAP',
+                price: '$5.50',
+                image: 'https://picsum.photos/182/264'
+            },
+            {
+                id: '{{objectId()}}',
+                title: 'Top Men Slim-Fit',
+                brand: 'Gucci',
+                price: '$6.68',
+                image: 'https://picsum.photos/182/264'
+            },
+            {
+                id: '{{objectId()}}',
+                title: 'Fruit of the Loom Girls Top',
+                brand: 'Gucci',
+                price: '$25.54',
+                image: 'https://picsum.photos/182/264'
+            }
+        ]
+    },
+    {
+        id: '{{objectId()}}',
+        keyword: 'top tan',
+        suggestionTerms: [
+            {
+                term: 'top tank with bra',
+                url: '#'
+            },
+            {
+                term: 'top tank for woman',
+                url: '#'
+            },
+            {
+                term: 'top tank for men',
+                url: '#'
+            },
+            {
+                term: 'top tablets',
+                url: '#'
+            }
+        ],
+        collections: [
+            {
+                id: '{{objectId()}}',
+                title: 'Tops',
+                url: '#'
+            }
+        ],
+        products: [
+            {
+                id: '{{objectId()}}',
+                title: 'Essentials Women Slim-Fit Tank',
+                brand: 'GAP',
+                price: '$5.50',
+                image: 'https://picsum.photos/182/264'
+            },
+            {
+                id: '{{objectId()}}',
+                title: 'Top Men Slim-Fit',
+                brand: 'Gucci',
+                price: '$6.68',
+                image: 'https://picsum.photos/182/264'
+            },
+            {
+                id: '{{objectId()}}',
+                title: 'Fruit of the Loom Girls Top',
+                brand: 'Gucci',
+                price: '$25.54',
+                image: 'https://picsum.photos/182/264'
+            }
+        ]
+    },
+    {
+        id: '{{objectId()}}',
+        keyword: 'top tank',
+        suggestionTerms: [
+            {
+                term: 'top tank with bra',
+                url: '#'
+            },
+            {
+                term: 'top tank for woman',
+                url: '#'
+            },
+            {
+                term: 'top tank for men',
+                url: '#'
+            },
+            {
+                term: 'top tablets',
+                url: '#'
+            }
+        ],
+        collections: [
+            {
+                id: '{{objectId()}}',
+                title: 'Tops',
+                url: '#'
+            }
+        ],
+        products: [
+            {
+                id: '{{objectId()}}',
+                title: 'Essentials Women Slim-Fit Tank',
+                brand: 'GAP',
+                price: '$5.50',
+                image: 'https://picsum.photos/182/264'
+            },
+            {
+                id: '{{objectId()}}',
+                title: 'Top Men Slim-Fit',
+                brand: 'Gucci',
+                price: '$6.68',
+                image: 'https://picsum.photos/182/264'
+            },
+            {
+                id: '{{objectId()}}',
+                title: 'Fruit of the Loom Girls Top',
+                brand: 'Gucci',
+                price: '$25.54',
+                image: 'https://picsum.photos/182/264'
+            }
+        ]
     }
 ]
