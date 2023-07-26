69ab18bb3c6b695498e1fda2224b39c47a0eb914
Tran Quoc Tuan <tuantran1295@MacBook-Air-cua-Tran.local>
2023-07-26 17:40:21 +0700

    add toggle show

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
 import SearchWithSuggestion from "./components/SearchWithSuggestion";
+import {useState} from "react";
 
 function App() {
+    const [searchKeyword, setKeyword] = useState('');
   return (
     <div className="App">
       <div className="flex min-h-screen justify-center overflow-hidden bg-gradient-to-br from-sky-500 via-teal-300 to-cyan-500">
-          <SearchWithSuggestion/>
+          <div className="translate-y-1/6">
+              <div className="relative w-96 max-w-lg">
+                  <form>
+                      <div className="flex justify-between overflow-hidden rounded-md bg-white shadow shadow-black/20">
+                          <input type="text"
+                                 className="block w-full flex-1 py-2 px-3 focus:outline-none"
+                                 placeholder="Search Product..."
+                                 onChange={(e) => {
+                                     setKeyword(e.target.value)
+                                 }}
+                          />
+                          <span
+                              className="m-1 inline-flex cursor-pointer items-center rounded-md bg-indigo-600 px-2 py-2 hover:bg-indigo-700">
+                            <svg className="text-white" width="32" height="32" viewBox="0 0 24 24">
+                            <path fill="currentColor"
+                                  d="M21.07 16.83L19 14.71a3.08 3.08 0 0 0-3.4-.57l-.9-.9a7 7 0 1 0-1.41 1.41l.89.89a3 3 0 0 0 .53 3.46l2.12 2.12a3 3 0 0 0 4.24 0a3 3 0 0 0 0-4.29Zm-8.48-4.24a5 5 0 1 1 0-7.08a5 5 0 0 1 0 7.08Zm7.07 7.07a1 1 0 0 1-1.42 0l-2.12-2.12a1 1 0 0 1 0-1.42a1 1 0 0 1 1.42 0l2.12 2.12a1 1 0 0 1 0 1.42Z"/></svg>
+                        </span>
+                      </div>
+                  </form>
+
+                  <SearchWithSuggestion
+                      keyword={searchKeyword}
+                      minKeywordLength={1}
+                  />
+              </div>
+          </div>
       </div>
     </div>
   );
--- src/components/SearchWithSuggestion.js
+++ src/components/SearchWithSuggestion.js
@@ -1,16 +1,19 @@
 import {useEffect, useState} from "react";
 import searchResult from "../json/SearchResult.json"
 
-const SearchWithSuggestion = ({minKeywordLength = 1}) => {
-    const [searchKeyword, setKeyword] = useState('');
+const SearchWithSuggestion = ({keyword = '',minKeywordLength = 1}) => {
+
     const [suggestions, setSuggestions] = useState([]);
     const [collections, setCollections] = useState([]);
     const [products, setProducts] = useState([]);
+    const [isShowSuggestion, setShowSuggest] = useState(true);
+    const [isShowCollection, setShowCollect] = useState(true);
+    const [isShowProduct, setShowProduct] = useState(true);
 
     useEffect(() => {
         for (let i = 0; i < searchResult.length; i++) {
             const currentResult = searchResult[i];
-            if (searchKeyword === currentResult.keyword) {
+            if (keyword === currentResult.keyword) {
                 setSuggestions(currentResult.suggestionTerms);
                 setCollections(currentResult.collections);
                 setProducts(currentResult.products);
@@ -21,54 +24,61 @@ const SearchWithSuggestion = ({minKeywordLength = 1}) => {
                 setProducts([]);
             }
         }
-    }, [searchKeyword])
+    }, [keyword])
+
+    const toggleShowSuggest = () => {
+        setShowSuggest((prevState) => {
+            return !prevState;
+        })
+    }
+
+    const toggleShowCollect = () => {
+        setShowCollect((prevState) => {
+            return !prevState;
+        })
+    }
+
+    const toggleShowProduct = () => {
+        setShowProduct((prevState) => {
+            return !prevState;
+        })
+    }
 
     return (
         <div className="translate-y-1/6">
             <div className="relative w-96 max-w-lg">
-                <form>
-                    <div className="flex justify-between overflow-hidden rounded-md bg-white shadow shadow-black/20">
-                        <input type="text"
-                               className="block w-full flex-1 py-2 px-3 focus:outline-none"
-                               placeholder="Search Product..."
-                               onChange={(e) => {
-                                   setKeyword(e.target.value)
-                               }}
-                        />
-                        <span
-                            className="m-1 inline-flex cursor-pointer items-center rounded-md bg-indigo-600 px-2 py-2 hover:bg-indigo-700">
-                            <svg className="text-white" width="32" height="32" viewBox="0 0 24 24">
-                            <path fill="currentColor"
-                                  d="M21.07 16.83L19 14.71a3.08 3.08 0 0 0-3.4-.57l-.9-.9a7 7 0 1 0-1.41 1.41l.89.89a3 3 0 0 0 .53 3.46l2.12 2.12a3 3 0 0 0 4.24 0a3 3 0 0 0 0-4.29Zm-8.48-4.24a5 5 0 1 1 0-7.08a5 5 0 0 1 0 7.08Zm7.07 7.07a1 1 0 0 1-1.42 0l-2.12-2.12a1 1 0 0 1 0-1.42a1 1 0 0 1 1.42 0l2.12 2.12a1 1 0 0 1 0 1.42Z"/></svg>
-                        </span>
-                    </div>
-                </form>
 
-                {searchKeyword.length >= minKeywordLength &&
+                {keyword.length >= minKeywordLength &&
                     <div className="max-h-[90vh] overflow-y-auto absolute mt-2 w-full rounded-md bg-white">
-                        <div className="cursor-pointer py-2 px-2 bg-slate-100">
+                        <div className="cursor-pointer py-2 px-2 bg-slate-100"
+                             onClick={toggleShowSuggest}
+                        >
                             <p className="text-sm font-medium text-gray-300 uppercase">Suggestions</p>
                         </div>
 
-                        {suggestions.map((suggest, index) =>
+                        {isShowSuggestion && suggestions.map((suggest, index) =>
                             <div key={index} className="cursor-pointer py-2 px-3 hover:bg-slate-100">
                                 <p className="text-sm text-gray-500">{suggest.term}</p>
                             </div>
                         )}
-                        <div className="cursor-pointer py-2 px-2 bg-slate-100">
+                        <div className="cursor-pointer py-2 px-2 bg-slate-100"
+                             onClick={toggleShowCollect}
+                        >
                             <p className="text-sm font-medium text-gray-300 uppercase">Collections</p>
                         </div>
 
-                        {collections.map((collect, index) =>
+                        {isShowCollection && collections.map((collect, index) =>
                             <div key={index} className="cursor-pointer py-2 px-3 hover:bg-slate-100">
                                 <p className="text-sm text-gray-500">{collect.title}</p>
                             </div>
                         )}
-                        <div className="cursor-pointer py-2 px-2 bg-slate-100">
+                        <div className="cursor-pointer py-2 px-2 bg-slate-100"
+                             onClick={toggleShowProduct}
+                        >
                             <p className="text-sm font-medium text-gray-300 uppercase">Products</p>
                         </div>
 
-                        {products.map((product, index) =>
+                        {isShowProduct && products.map((product, index) =>
                             <div key={index} className="flex flex-row flex-auto hover:bg-slate-100">
                                 <img className="w-1/5 h-auto px-3 py-2 rounded-md"
                                      src={product.image}
@@ -83,7 +93,6 @@ const SearchWithSuggestion = ({minKeywordLength = 1}) => {
                         )}
 
                     </div>}
-
             </div>
         </div>
     )
